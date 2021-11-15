2ca75e476f297848023372521817a07407d6ebef
comaka <colin.kargl@gmail.com>
2021-11-15 04:10:43 +0100

    formatting

--- LapTimeCalculator/Program.cs
+++ LapTimeCalculator/Program.cs
@@ -8,11 +8,11 @@ namespace LapTimeCalculator
         {
             Console.WriteLine("Forza Horizon 5 - Time Calculator");
 
-            var laps = ReadNumberWithLimit("\nHow many laps does the race consist of? (1-50)", 50, 1);                    // Definition for the amount of Laps
+            var laps = ReadNumberWithLimit("\nHow many laps does the race consist of? (1-50) ", 50, 1);                    // Definition for the amount of Laps
 
-            var minutes = ReadNumberWithLimit("\nHow many minutes does a single lap take? (0-60)", 60, 0);   // Definition of the time a single Lap is taking (Minute Counter)
+            var minutes = ReadNumberWithLimit("\nHow many minutes does a single lap take? (0-60) ", 60, 0);   // Definition of the time a single Lap is taking (Minute Counter)
 
-            var seconds = ReadNumberWithLimit("\nHow many seconds does a single lap take? (0-60)", 60, 0);                // Definition of the time a single Lap is taking (Second Counter)
+            var seconds = ReadNumberWithLimit("\nHow many seconds does a single lap take? (0-60) ", 60, 0);                // Definition of the time a single Lap is taking (Second Counter)
 
             var lapTime = (minutes * 60) + seconds;                                                                       // Calculation of the Output Time
             var finalTime = TimeSpan.FromSeconds(laps * lapTime);
