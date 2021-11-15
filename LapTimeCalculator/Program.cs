3a443fc9a8d0785929ba507b4958e0443ef75bdb
ItzCelyrian <75842182+ItzCelyrian@users.noreply.github.com>
2021-11-15 03:16:51 +0100

    Add files via upload

--- LapTimeCalculator/Program.cs
+++ LapTimeCalculator/Program.cs
@@ -6,57 +6,41 @@ namespace LapTimeCalculator
     {
         private static void Main()
         {
-            Console.WriteLine("Forza Horizon 5 - Time Calculator");
-            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Forza Horizon 5 - Time Calculator\n");
 
             // Definition for the amount of Laps
-            Console.WriteLine("How many Laps does the race consist of?");
+            Console.WriteLine("How many Laps does the race consist of?\n");
             var laps = Convert.ToInt64(Console.ReadLine());
-
-            // Spacer
-            Console.WriteLine("");
             
             // Definition of the time a single Lap is taking (Minute Counter)
-            Console.WriteLine("How many Minutes does a single Lap have?");
+            Console.WriteLine("How many Minutes does a single Lap have?\n");
             var minutes = Convert.ToDouble(Console.ReadLine());
             
-            // Spacer
-            Console.WriteLine("");
-            
             // Definition of the time a single Lap is taking (Second Counter)
-            Console.WriteLine("How many Seconds does a single Lap have? (Decimals are given with ',')");
+            Console.WriteLine("How many Seconds does a single Lap have? (Decimals are given with ',')\n");
             var seconds = Convert.ToDouble(Console.ReadLine());
             
-            // Spacer
-            Console.WriteLine("");
-            
-            // Definition of the Vehicle the player is in.
-            Console.WriteLine("Which Vehicle are you using for the given race?");
-            var vehicle = Convert.ToString(Console.ReadLine());
-            
-            // Spacer
-            Console.WriteLine("");
-            
             // Cases for if laps and time is exceeding the limit
-            if (laps is > 50 or < 0)
+            if (laps is > 50 or <= 0)
             {
                 laps = 1;
-                Console.WriteLine("Your given laps were invalid, the lap-counter was automatically adjusted to '1'.");
-                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your given laps were invalid, the lap-counter was automatically adjusted to '1'.\n");
             }
             
-            if (minutes is >= 60 or <= 0)
+            if (minutes is >= 60 or < 0)
             {
                 minutes = 1;
-                Console.WriteLine("Your given minutes were invalid, the minute-counter was automatically adjusted to '1'.");
-                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your given minutes were invalid, the minute-counter was automatically adjusted to '1'.\n");
             }
 
-            if (seconds is >= 60 or <= 0)
+            if (seconds is >= 60 or < 0)
             {
                 seconds = 1;
-                Console.WriteLine("Your given seconds were invalid, the second-counter was automatically adjusted to '1'.");
-                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your given seconds were invalid, the second-counter was automatically adjusted to '1'.\n");
             }
 
             // Calculation of the Output Time
@@ -65,11 +49,10 @@ namespace LapTimeCalculator
             var outputTime = finalTime .ToString(@"hh\:mm\:ss\:fff");
             
             // Output of the Variable after the calculation
-            Console.WriteLine($"The {vehicle} will take {outputTime} to drive {laps} Laps.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"The Race will take {outputTime} to be completed within {laps} Laps.\n");
             
-            // Spacer
-            Console.WriteLine("");
-            
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Press any Key to exit...");
             Console.ReadLine();
         }
