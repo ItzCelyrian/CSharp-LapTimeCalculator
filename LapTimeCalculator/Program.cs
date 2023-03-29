c79c6f75b582edf9d0a09a5ce7c052ab9e8717b5
ItzCelyrian <75842182+ItzCelyrian@users.noreply.github.com>
2023-03-29 10:00:01 +0200

    Update Program.cs
    Exception Habdling improved

--- LapTimeCalculator/Program.cs
+++ LapTimeCalculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LapTimeCalculator
 {
@@ -11,6 +11,16 @@ namespace LapTimeCalculator
             var laps = ReadNumberWithLimit("\nHow many laps does the race consist of? (1-50) ", 50, 1);
             var minutes = ReadNumberWithLimit("\nHow many minutes does a single lap take? (0-60) ", 60, 0);
             var seconds = ReadNumberWithLimit("\nHow many seconds does a single lap take? (0-60) ", 60, 0);
+            
+            while (minutes == 0 && seconds == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A lap should take at least 1 second.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                minutes = ReadNumberWithLimit("\nHow many minutes does a single lap take? (0-60) ", 60, 0);
+                seconds = ReadNumberWithLimit("\nHow many seconds does a single lap take? (0-60) ", 60, 0);
+            }
+            
             var lapTime = (minutes * 60) + seconds;
             var finalTime = TimeSpan.FromSeconds(laps * lapTime);
             var outputTime = finalTime.ToString(@"hh\:mm\:ss");
@@ -24,29 +34,21 @@ namespace LapTimeCalculator
 
         private static int ReadNumberWithLimit(string prompt, int maxValue = Int32.MaxValue, int minValue = Int32.MinValue)
         {
-            var returnVal = 0;
-            var isCorrect = false;
-            do
+            int returnVal;
+
+            while (true)
             {
                 Console.Write(prompt);
 
-                try
+                if (int.TryParse(Console.ReadLine(), out returnVal) && returnVal >= minValue && returnVal <= maxValue)
                 {
-                    returnVal = int.Parse(Console.ReadLine() ?? string.Empty);
-                }
-                catch
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    continue;
+                    break;
                 }
 
                 Console.ForegroundColor = ConsoleColor.Red;
-                isCorrect = returnVal >= minValue && returnVal <= maxValue;
-                if (!isCorrect) Console.WriteLine("Please enter a valid number within the given limits.");
+                Console.WriteLine("Please enter a valid number within the given limits.");
                 Console.ForegroundColor = ConsoleColor.Gray;
-            } while (!isCorrect);
+            }
 
             return returnVal;
         }
